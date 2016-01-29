d843015a7ce6b5125eac758337eda2a6206a0222
tewksbum <marcus@mobywize.com>
2016-01-28 19:45:59 -0500

    first changes...

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-    name: 'matteodem:server-session',
-    summary : "Serverside Session through a Meteor.Collection (get, set, equals etc.)",
-    version: "0.4.2",
-    git: "https://github.com/matteodem/meteor-server-session.git"
+    name: 'tewksbum:server-session',
+    summary : "Serverside Session through a Meteor.Collection (get, set, equals etc.) w/ check",
+    version: "0.0.0",
+    git: "https://github.com/tewksbum/meteor-server-session"
 });
 
 Package.on_use(function (api) {
     api.versionsFrom('METEOR@0.9.0');
 
     api.use('underscore', 'client');
-    api.use('underscore', 'server');
+    api.use(['underscore', 'check'], 'server');
     api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
 
     api.export('ServerSession');
